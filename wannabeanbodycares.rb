f260e41e8be3d078eb351bad3d1262122cf6d502
austinfulks <54602734+austinfulks@users.noreply.github.com>
2020-01-21 13:38:31 -0800

    Add files via upload

---
+++ wannabeanbodycares.rb
@@ -0,0 +1,21 @@
+# Welcome to Sonic Pi v3.1
+live_loop :gyup_up do
+  play:f4
+  sleep 0.2
+  play:g4
+  sleep 0.2
+  play:e4
+  sleep 0.2
+  play:c4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:a4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+end
